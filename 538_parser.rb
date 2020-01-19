f02412a5dce8f7cfb79a64d6dd6f7fffb0665f43
Tom Bedor <tombedor@gmail.com>
2020-01-19 14:10:42 -0800

    update

--- 538_parser.rb
+++ 538_parser.rb
@@ -5,23 +5,26 @@ driver = Selenium::WebDriver.for :firefox
 
 SITE = "https://projects.fivethirtyeight.com/2020-primary-forecast/"
 STATES = File.read('states.txt').split("\n").map{|s| s.downcase.gsub(" ", "-")}
+STATES = STATES[36..-1]
 CANDIDATES = "Sanders,Warren,Biden,Buttigieg,Bloomberg,Klobuchar,Steyer,Yang,Gabbard,Booker,Harris,Delaney,Patrick,Castro,Bullock,Williamson,Bennet,Sestak".split(",")
-CSV.open("538_output.csv", "a") do |csv|
-	HEADER = %w(state candidate percent_chance date url)
+CSV.open("data/538_output_#{Date.today}.csv", "a") do |csv|
+	HEADER = %w(source state candidate yes_price no_price date url)
 	csv << HEADER
 	STATES.each do |state|
 		url = SITE + state
 		puts "scraping #{url}"
 		driver.navigate.to(url)
-		sleep 5
+		sleep 10
 		driver.find_element(:class, "candidate-select").find_elements(tag_name: "option").each do |option|
 			option.click
-			sleep 1
+			sleep 2
 			name = option.attribute("innerText")
 			puts name
 			next if driver.find_element(class: "robo-text").attribute("innerText").include?("is no longer actively campaigning")
 			odds = driver.find_elements(tag_name: 'c')[0].attribute("innerText")[1...-2]
-			csv << [state, name, odds, Date.today, url]
+			yes_price = odds.to_f / 100
+			no_price = 1 - yes_price
+			csv << [state, name, yes_price, no_price, Date.today, url]
 		end
 	end
 end
--- predictit_parser.rb
+++ predictit_parser.rb
@@ -15,22 +15,22 @@ PRICES_SCRIPT = (<<-EOF).strip
 return $.map($('.row.row-100.market-contract-horizontal-v2__row'), function(element) {name = $(element).find('.market-contract-horizontal-v2__title-text')[0].innerHTML;yesPrice = $(element).find('.button-price__price')[0].innerHTML;noPrice = $(element).find('.button-price__price')[1].innerHTML;return name + ',' + yesPrice + ',' + noPrice}).join(';')
 EOF
 
-HEADER = %w(state candidate yes_price no_price date url)
-CSV.open("predictit_output.csv", "a") do |csv|
+HEADER = %w(source state candidate yes_price no_price date url)
+CSV.open("data/predictit_output_#{Date.today}.csv", "a") do |csv|
 	csv << HEADER
-	p_urls.each do |url|
+	p_urls[0..2].each do |url|
 		puts "parsing #{url}"
 		driver.navigate.to url
-		sleep 5
+		sleep 8
 		raw_prices = driver.execute_script(PRICES_SCRIPT)
 		raw_title = driver.execute_script(TITLE_SCRIPT)
-		state = raw_title.gsub("Who will win the 2020 ", "").gsub(/ Democratic.*/, "")
+		state = raw_title.gsub("Who will win the 2020 ", "").gsub(/ Democratic.*/, "").downcase.gsub(' ', '-')
 
 		raw_prices.split(";") do |row|
-			candidate = row.split(",")[0]
+			candidate = row.split(",")[0].split()[-1]
 			yes_price = "0." + row.split(",")[1].gsub("¢", "")
 			no_price = "0." + row.split(",")[2].gsub("¢", "")
-			csv << [state, candidate, yes_price, no_price, Date.today, url]
+			csv << ["predictit", state, candidate, yes_price, no_price, Date.today, url]
 		end
 	end
 end
